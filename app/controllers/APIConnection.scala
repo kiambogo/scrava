13d1b8c3221c3569ba0288ed099d6e069ec373f4
Christopher Poenaru <kiambogo@gmail.com>
2014-09-19 13:13:24 -0400

    added models and first API calls

--- app/controllers/APIConnection.scala
+++ app/controllers/APIConnection.scala
@@ -4,34 +4,44 @@ package controllers
  * Created by christopher on 2014-09-15.
  */
 
-import java.util.concurrent.TimeUnit
-import models.Athlete
+import models.{SegmentEffort, Athlete}
 import play.api.libs.ws.WS
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+
+import scala.concurrent.Future
 
 
 case class Connection(
                        accessToken: String,
                        athlete: Athlete) {
 
-  def updateAthlete(parameters: Map): Athlete = {
-    val authString = "Bearer " + accessToken
-    val future = WS.url("https://www.strava.com/api/v3/athlete")
+  val authString = "Bearer " + accessToken
+
+  def updateAthlete(parameters: Map): Future[Athlete] = {
+    WS.url("https://www.strava.com/api/v3/athlete")
       .withHeaders("Authorization" -> authString)
       .put(parameters)
       .map { response =>
       response.json.validate[Athlete].get
     }
-    Await.result(future, Duration(3, TimeUnit.SECONDS))
   }
 
-  def findAthlete(id: Int): Option[Athlete] = {
-    val future_athlete = WS.url("https://www.strava.com/api/v3/athlete/" + id)
+  def findAthlete(id: Int): Future[Option[Athlete]] = {
+    WS.url(s"https://www.strava.com/api/v3/athlete/$id")
       .get()
-      .map( response =>
+      .map { response =>
       response.json.validate[Athlete].asOpt
+    }
+  }
+
+  def listAthleteKOMs(id: Int, page: Int, resultsPerPage: Int): Future[List[SegmentEffort]] = {
+    WS.url(s"https://www.strava.com/api/v3/athletes/$id/koms")
+      .withHeaders("Authorization" -> authString)
+      .get()
+      .map { response =>
+      response.json.validate[List[SegmentEffort]].fold(
+        errors => throw new RuntimeException("Could not parse list of KOMs"),
+        koms => koms
       )
-    Await.result(future_athlete, Duration(3, TimeUnit.SECONDS))
+    }
   }
 }
