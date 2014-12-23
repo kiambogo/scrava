b0f5b17fd6a5ee72898dcc5b08202a2521f6528d
Christopher Poenaru <kiambogo@gmail.com>
2014-12-23 01:20:57 -0500

    started writing functional tests

--- build.sbt
+++ build.sbt
@@ -12,5 +12,6 @@ libraryDependencies ++= Seq(
   cache,
   ws,
   "net.liftweb" %% "lift-json" % "3.0-M2",
-  "org.scalatest" %% "scalatest" % "2.2.2"
+  "org.scalatest" %% "scalatest" % "2.2.2",
+  "org.scalatestplus" %% "play" % "1.2.0" % "test"
 )
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,24 +1,53 @@
-import org.specs2.mutable._
-import org.specs2.runner._
 import org.junit.runner._
+import org.scalatestplus.play._
+import org.specs2.runner._
+import scrava.controllers._
+import scrava.models.{GearSummary, DetailedAthlete}
 
-import play.api.test._
-import play.api.test.Helpers._
+import scala.concurrent.{duration, Await}
+import scala.concurrent.duration.Duration
+
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
 @RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends PlaySpec with OneAppPerSuite {
+
+  val testToken = "21b4fe41a815dd7de4f0cae7f04bbbf9aa0f9507"
+  val testClient = new ScravaClient(testToken)
 
   "Application" should {
 
-    "work from within a browser" in new WithBrowser {
-
-      browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
+    "retrieve Athlete profile" in {
+      val futureAth = testClient.retrieveAthlete()
+      val ath = Await.result(futureAth, Duration(5, duration.SECONDS)).merge.asInstanceOf[DetailedAthlete]
+      ath.firstname must equal("Strava")
+      ath.lastname must equal("Testing")
+      ath.email must equal("paul+test@strava.com")
     }
+
+    "retrieve friends" in {
+      val futureFriends = testClient.listAthleteFriends()
+      val friends = Await.result(futureFriends, Duration(5, duration.SECONDS))
+      friends.size must equal(2)
+    }
+
+    "retrieve gear" in {
+      val futureAth = testClient.retrieveAthlete()
+      val ath = Await.result(futureAth, Duration(5, duration.SECONDS)).merge.asInstanceOf[DetailedAthlete]
+      val gear: List[GearSummary] = ath.bikes ::: ath.shoes
+      gear.size must equal(0)
+    }
+
+    "retrieve activity" in {
+      val futureAth = testClient.listAthleteActivities()
+      val ath = Await.result(futureAth, Duration(5, duration.SECONDS))
+
+      ath.size must equal(7)
+    }
+
+
   }
 }
