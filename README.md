03755bd82e19650e2d35cfcc0232c77ada564e80
Christopher Poenaru <kiambogo@gmail.com>
2014-12-23 15:41:05 -0500

    added and revised tests, updated README

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ An asynchronous, non-blocking  Scala client for the Strava API built upon Play's
 [![Build Status](https://travis-ci.org/kiambogo/Scrava.svg?branch=master)](https://travis-ci.org/kiambogo/Scrava)
 
 ###Using the getAll higher order function
-The Strava API offers the ability to page through result sets when the corresponding result to a request has many results. Scrava offers the ability to fetch all and merge into a single List. To do this, call upon the *getAll()* higher order function, passing into it the function you wish to invoke. You can pass in other required parameters as needed.
+The Strava API offers the ability to page through result sets when the corresponding result to a request has many results. Scrava offers the ability to fetch all and merge into a single List. To do this, call upon the `getAll()` higher order function, passing into it the function you wish to invoke. You can pass in other required parameters as needed.
 
 For example, if you want t retrieve all the activities of the current athlete
 ```scala
@@ -13,6 +13,13 @@ import scrava.controllers._
 import scrava.models._
 AutoPagination.getAll(client.listAthleteActivities _)
 ```
+
+or if you want to list all the athletes you are mutually following with a particular athlete:
+
+```scala
+AutoPagination.getAll(client.listMutualFollowing _, athlete_id)
+```
+where athlete_id is the particular Athlete ID you wish to find mutual followers for, and is a parameter to the `listMutualFollowing()` function.
 
 The underscore at the end marks it as a partially applied function, leaving the rest of the parameters to be curried. 
 
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -2,18 +2,13 @@ import org.junit.runner._
 import org.scalatestplus.play._
 import org.specs2.runner._
 import scrava.controllers._
-import scrava.models.{GearSummary, DetailedAthlete}
+import scrava.models._
 
 import scala.concurrent.{duration, Await}
 import scala.concurrent.duration.Duration
 
-
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
 @RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends PlaySpec with OneAppPerSuite {
+class IntegrationTest extends PlaySpec with OneAppPerSuite {
 
   val testToken = "21b4fe41a815dd7de4f0cae7f04bbbf9aa0f9507"
   val testClient = new ScravaClient(testToken)
@@ -31,22 +26,67 @@ class IntegrationSpec extends PlaySpec with OneAppPerSuite {
     "retrieve friends" in {
       val futureFriends = testClient.listAthleteFriends()
       val friends = Await.result(futureFriends, Duration(5, duration.SECONDS))
-      friends.size must equal(2)
+      (friends map (_.id == 227615) contains true) must equal(true)
     }
 
     "retrieve gear" in {
       val futureAth = testClient.retrieveAthlete()
       val ath = Await.result(futureAth, Duration(5, duration.SECONDS)).merge.asInstanceOf[DetailedAthlete]
+
       val gear: List[GearSummary] = ath.bikes ::: ath.shoes
       gear.size must equal(0)
     }
 
-    "retrieve activity" in {
-      val futureAth = testClient.listAthleteActivities()
-      val ath = Await.result(futureAth, Duration(5, duration.SECONDS))
-
-      ath.size must equal(7)
+    "retrieve activities" in {
+      val futureActivities = testClient.listAthleteActivities()
+      val activities = Await.result(futureActivities, Duration(5, duration.SECONDS))
+      (activities map (_.id == 118293263) contains true) must equal(true)
     }
+
+    "retrieve followers" in {
+      val futureFollowers = testClient.listAthleteFollowers()
+      val followers = Await.result(futureFollowers, Duration(5, duration.SECONDS))
+      (followers map (_.id == 227615) contains true) must equal(true)
+    }
+
+    "retrieve mutual followings" in {
+      val futureFollowers = testClient.listMutualFollowing(227615)
+      val followers = Await.result(futureFollowers, Duration(5, duration.SECONDS))
+      (followers map (_.id == 8758) contains true) must equal(true)
+    }
+
+    "retrieve KOMs" in {
+      val futureAth = testClient.retrieveAthlete()
+      val ath = Await.result(futureAth, Duration(5, duration.SECONDS)).merge.asInstanceOf[DetailedAthlete]
+
+      val futureKoms = testClient.listAthleteKOMs(ath.id)
+      val koms = Await.result(futureKoms, Duration(5, duration.SECONDS))
+      koms.size must equal(0)
+    }
+
+    "retrieve activity comments" in {
+      val futureComments = testClient.listActivityComments(118293263)
+      val comments = Await.result(futureComments, Duration(5, duration.SECONDS))
+      (comments map (_.text == "test comment") contains true) must equal(true)
+    }
+
+    "retrieve activity kudoers " in {
+      val futureKudoers = testClient.listActivityKudoers(118293263)
+      val kudoers = Await.result(futureKudoers, Duration(5, duration.SECONDS))
+      (kudoers map (_.id == 3776) contains true) must equal(true)
+    }
+
+//    "retrieve activity details " in {
+//      val futureActivity = testClient.retrieveActivity(118293263)
+//      val activity = Await.result(futureActivity, Duration(5, duration.SECONDS)).merge.asInstanceOf[Activity]
+//      activity.id must equal(118293263)
+//    }
+
+//    "retrieve activity photos " in {
+//      val futurePhotos = testClient.listActivityPhotos(118293263)
+//      val photos = Await.result(futurePhotos, Duration(5, duration.SECONDS))
+//      photos.size must equal(0)
+//    }
 
 
   }
